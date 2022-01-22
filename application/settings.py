a91add5372bb4e12a4dbb9e0aad2a9423b69482c
ottomayerpy <fivesevenom@gmail.com>
2022-01-22 22:24:10 +0300

    update

--- application/settings.py
+++ application/settings.py
@@ -2,8 +2,8 @@ import os
 
 DEBUG = True
 
-PG_USER = os.getenv('POSTGRES_USER')
-PG_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-PG_DB = os.getenv('POSTGRES_DB')
+PG_USER = os.getenv('PGUSER')
+PG_PASSWORD = os.getenv('PGPASSWORD')
+PG_DB = os.getenv('PGDATABASE')
 PG_HOST = 'db'
 PG_PORT = 5432
